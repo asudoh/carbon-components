61fb85c6dd84addcfcb1726d2ee511af38c13c48
Akira Sudoh <asudoh@gmail.com>
2020-07-06 09:50:04 +0900

    chore(examples): update React CodeSandbox

--- packages/react/examples/codesandbox/src/index.js
+++ packages/react/examples/codesandbox/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Button } from 'carbon-components-react';
+import Button from 'carbon-components-react/es/components/Button/Button';
+import './index.scss';
 
 const App = () => (
   <div>
